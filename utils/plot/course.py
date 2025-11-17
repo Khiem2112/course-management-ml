08037c6b66423c11dfa9ecb0fd0486601167bb82
dotan915 <dotan915@gmail.com>
2025-11-17 19:35:42 +0700

    Fix dropout pie chart bug

--- utils/plot/course.py
+++ utils/plot/course.py
@@ -1,16 +1,25 @@
-from database.course.course import get_dropout_percentage
-from utils.plot.plot_manager import PlotManager, MplCanvas
 from PyQt6.QtWidgets import QWidget
 
+from utils.plot.plot_manager import PlotManager
+
+
 class CourseInfoVisualizer():
-  @staticmethod
-  def create_dropout_rate_pie(data: dict, target_widget: QWidget) -> PlotManager:
-    
-    canvas = PlotManager._find_or_create_canvas(target_widget=target_widget)
-    
-    ax = canvas.axes
-    fig = canvas.figure
-    
-    drop_out_dict = data
-    ax.pie(x=drop_out_dict.values(), labels= drop_out_dict.keys(), autopct="%1.1f%%")
-    return PlotManager(canvas)
+    @staticmethod
+    def create_dropout_rate_pie(data: dict, target_widget: QWidget) -> PlotManager:
+        canvas = PlotManager._find_or_create_canvas(target_widget=target_widget)
+
+        ax = canvas.axes
+        fig = canvas.figure
+
+        drop_out_dict = data
+
+        # CHUYỂN Decimal → float, dict_values → list
+        values = [float(v) for v in drop_out_dict.values()]
+        labels = list(drop_out_dict.keys())
+
+        ax.clear()  # tránh vẽ chồng
+        ax.pie(values, labels=labels, autopct="%1.1f%%")
+
+        fig.tight_layout()
+
+        return PlotManager(canvas)
